644d351d9fdb2f7b159f6aaca66b6d262700721c
SofiaMartinez23 <sofiamg2392001@gmail.com>
2025-04-12 16:45:33 +0200

    Add project files.

---
+++ NugetViajesSMG/Models/UsuarioCompletoView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace NugetViajesSMG.Models
+{
+    [Table("VISTAUSUARIOCOMPLETO")] 
+    public class UsuarioCompletoView
+    {
+        [Key]
+        [Column("ID_USUARIO")]
+        public int IdUsuario { get; set; }
+
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+
+        [Column("EMAIL")]
+        public string Email { get; set; }
+
+        [Column("EDAD")]
+        public int Edad { get; set; }
+
+        [Column("NACIONALIDAD")]
+        public string Nacionalidad { get; set; }
+
+        [Column("PREFERENCIASDEVIAJE")]
+        public string PreferenciaViaje { get; set; } 
+
+        [Column("CLAVE")]
+        public string Clave { get; set; }
+
+        [Column("CONFIRMARCLAVE")]
+        public string ConfirmarClave { get; set; }
+
+        [Column("AVATARURL")]
+        public string AvatarUrl { get; set; }
+    }
+}
